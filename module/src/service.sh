4e62f70b2bac96e45d043b668b1c47eba2d18fa4
Thehepta <rizixi@foxmail.com>
2025-05-29 20:34:48 +0800

    add magisk compatible

---
+++ module/src/service.sh
@@ -0,0 +1,23 @@
+#!/system/bin/sh
+
+DEBUG=@DEBUG@
+
+MODDIR=${0%/*}
+if [ "$ZYGISK_ENABLED" ]; then
+  exit 0
+fi
+
+cd "$MODDIR"
+
+if [ "$(which magisk)" ]; then
+  for file in ../*; do
+    if [ -d "$file" ] && [ -d "$file/zygisk" ] && ! [ -f "$file/disable" ]; then
+      if [ -f "$file/service.sh" ]; then
+        cd "$file"
+        log -p i -t "zygisk-sh" "Manually trigger service.sh for $file"
+        sh "$(realpath ./service.sh)" &
+        cd "$MODDIR"
+      fi
+    fi
+  done
+fi
