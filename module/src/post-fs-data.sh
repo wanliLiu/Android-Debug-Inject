0f4b8ed65b49615d2a7a44a111aabec9a2ee6737
Thehepta <rizixi@foxmail.com>
2025-05-30 10:35:15 +0800

    add post-fs-data fix zygiskd permission

--- module/src/post-fs-data.sh
+++ module/src/post-fs-data.sh
@@ -1,6 +1,26 @@
 #!/system/bin/sh
 
+
 MODDIR=${0%/*}
+if [ "$ZYGISK_ENABLED" ]; then
+  exit 0
+fi
+
+cd "$MODDIR"
+
+if [ "$(which magisk)" ]; then
+  for file in ../*; do
+    if [ -d "$file" ] && [ -d "$file/zygisk" ] && ! [ -f "$file/disable" ]; then
+      if [ -f "$file/post-fs-data.sh" ]; then
+        cd "$file"
+        log -p i -t "zygisk-sh" "Manually trigger post-fs-data.sh for $file"
+        sh "$(realpath ./post-fs-data.sh)"
+        cd "$MODDIR"
+      fi
+    fi
+  done
+fi
+
 cd "$MODDIR" || exit
 
 chmod +x $MODDIR/bin -R
